9e9298f5276c3c7c7f5fffe0841fe1668da94d8b
poojabokare <pooja.h.chavan1992@gmail.com>
2023-07-28 10:18:08 +0530

    first commit demo

---
+++ src/git/commands/GitDemo.java
@@ -0,0 +1,8 @@
+package git.commands;
+
+public class GitDemo {
+public static void main(String[] args) {
+	System.out.println("by using git commands");
+	System.out.println("trying to execute all the commands");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author madan
+ *
+ */
+module FirstProjectForPushGit {
+}
